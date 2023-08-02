fa82c5f453c36c6302bac1c97d14d15628f2c3a9
nazm0l <tanjirahmednazmul@gmail.com>
2023-08-02 06:47:34 +0600

    Project Setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const port: number = 3000;
+
+// Connect to MongoDB
+
+async function dbConnect() {
+  try {
+    await mongoose.connect("mongodb://0.0.0.0:27017/book-cat");
+    console.log("Connected to MongoDB");
+
+    app.listen(port, () => {
+      console.log(`Server started at port ${port}`);
+    });
+  } catch (err) {
+    console.log(`Error in Database`, err);
+  }
+}
+
+dbConnect();
